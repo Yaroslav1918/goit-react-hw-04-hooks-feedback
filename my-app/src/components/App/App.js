b15b22e6f06c6a33269e66dfd24af4297d916248
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2022-01-09 17:12:11 +0200

    bag-fix

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,56 +1,53 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import FeedbackOptions from "../FeedbackOptions";
 import Statistics from "../Statistics";
 import Section from "../Section";
 import GlobalStyle from "../../Style/globalStyles";
 
-class App extends Component {
-  state = {
+export default function App() {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  setValue = (key) => {
-    this.setState((state) => ({
+  let copyOfState = { ...state };
+  const { good, neutral, bad } = state;
+  const stateKeys = Object.keys(state);
+
+  const setValue = (key) => {
+    setState((state) => ({
+      ...copyOfState,
       [key]: state[key] + 1,
     }));
   };
-
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     return Math.trunc((good * 100) / total);
   };
 
-  countTotalFeedback = () => {
-    const state = this.state;
+  const countTotalFeedback = () => {
     return Object.values(state).reduce((acc, value) => acc + value, 0);
   };
 
-  render() {
-    const state = Object.keys(this.state);
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
-    return (
-      <>
-        <GlobalStyle />
-        <Section title="Please live feedback">
-          <FeedbackOptions onLeaveFeedback={this.setValue} options={state} />
+  const positiveFeedbackPercentage = countPositiveFeedbackPercentage();
+  const totalFeedback = countTotalFeedback();
 
-          <Statistics
-            goodValue={good}
-            neutralValue={neutral}
-            badValue={bad}
-            state={state}
-            positivePercentage={positiveFeedbackPercentage}
-            total={total}
-          />
-        </Section>
-      </>
-    );
-  }
+  return (
+    <>
+      <GlobalStyle />
+      <Section title="Please live feedback">
+        <FeedbackOptions onLeaveFeedback={setValue} options={stateKeys} />
+
+        <Statistics
+          goodValue={good}
+          neutralValue={neutral}
+          badValue={bad}
+          state={state}
+          positivePercentage={positiveFeedbackPercentage}
+          total={totalFeedback}
+        />
+      </Section>
+    </>
+  );
 }
-
-export default App;
